79d7844b38f978717bab9f2eb81b3bdf12ae4931
Bharath <bharathsimha747@gmail.com>
2026-08-20 23:02:29 +0530

    Sync V1 server.js with working budget-tracker

--- server.js
+++ server.js
@@ -1,41 +1,34 @@
 var http = require('http');
 var fs = require('fs');
-
 http.createServer(function (req, res) {
-  var file = req.url.replace(/^\//, '');
-  var fileNames = file.split('.');
-  var extension = fileNames.length > 1 ? fileNames[fileNames.length - 1] : 'html';
+  var file = req.url.replace("/", "");
+  var fileNames = file.split(".");
+  var extension = fileNames.length > 0 ? fileNames[fileNames.length-1] : 'text/html';
   var mimeType = getMIMEType(extension);
 
-  file = (file === '' || !['tracker', 'admin', 'loan', 'lend', 'tracker.js', 'manifest.json', 'images/logo.png', 'serviceworker.js', 'analytics'].includes(file)) ? 'tracker' : file;
-  file = (file === 'tracker' || file === 'admin' || file === 'analytics' || file === 'loan' || file === 'lend') ? file + '.html' : file;
+  file = (file === "" || !["tracker", "admin", "loan", "lend", "tracker.js", "manifest.json", "images/logo.png", "serviceworker.js", "analytics"].includes(file)) ? "tracker" : file;
+  file = (file === "tracker" || file === "admin" || file === "analytics" || file == "loan" || file == "lend") ? file + ".html" : file;
 
-  fs.readFile(file, function (err, data) {
-    console.log(file);
-
-    if (err) {
-      console.error('Error reading file:', file, err);
-      res.writeHead(err.code === 'ENOENT' ? 404 : 500, { 'Content-Type': 'text/plain; charset=utf-8' });
-      return res.end(err.code === 'ENOENT' ? 'Not Found' : 'Internal Server Error');
-    }
-
-    res.writeHead(200, { 'Content-Type': mimeType });
-    return res.end(data);
+  fs.readFile(file, function(err, data) {
+    console.log(file)
+    res.writeHead(200, {'Content-Type': mimeType});
+    res.write(data);
+    return res.end();
+    
   });
-}).listen(process.env.PORT || 4200);
+}).listen(process.env.PORT || 4200)
 
 function getMIMEType(type) {
   var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    jpeg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript',
-    json: 'application/json'
+      html: 'text/html',
+      txt: 'text/plain',
+      css: 'text/css',
+      gif: 'image/gif',
+      jpg: 'image/jpeg',
+      png: 'image/png',
+      svg: 'image/svg+xml',
+      js: 'application/javascript',
+      json: 'application/json'
   };
-  return mime[type] || mime.html;
+  return !mime[type] ? mime["html"] : mime[type];
 }
